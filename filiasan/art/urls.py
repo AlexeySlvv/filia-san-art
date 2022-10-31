3db35f5cb2e2bb6c2dd652879c3f1ffddfb11bd6
Alexey Solovyov <me@alexeyslvv.ru>
2022-10-31 10:34:49 +0300

    add artist_list page

---
+++ filiasan/art/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.artist_list, name='index'),
+    path('art/', views.artist_list, name='artist_list'),
+    path('art/<str:artist>', views.artist_detail, name='artist-detail'),
+]
--- filiasan/art/views.py
+++ filiasan/art/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import json
 
 # Create your views here.
+
+
+def artist_list(request):
+    with open('Art.json', encoding='utf-8', mode='r') as json_file:
+        data = json.load(json_file)
+        artists = sorted(data.keys())
+    return render(request, 'art/artist_list.html', context={
+        'artists': artists,
+    })
+
+
+def artist_detail(request, artist):
+    return render(request, 'art/artist_list.html', context={
+        'artists': [artist],
+    })
