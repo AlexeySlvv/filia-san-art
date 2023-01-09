4bcd27e02094fd5ca4f1ad38b3521964542927c5
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-09 11:35:07 +0300

    add ArtistModel, ArtModel

--- filiasan/art/models.py
+++ filiasan/art/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ArtistModel(models.Model):
+    name = models.CharField(max_length=1024, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ArtModel(models.Model):
+    artist = models.ForeignKey(ArtistModel, null=False, on_delete=models.CASCADE)
+    year = models.DateField(null=True, default=None)
+    desc = models.TextField(default='', null=False)
+    url = models.CharField(max_length=1024, null=False)
+    orig_id = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.desc
