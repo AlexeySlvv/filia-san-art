9e57e6a4842383f3746b7d308b20037443f69c67
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-10 12:44:16 +0300

    update anything models

--- filiasan/app_anything/models.py
+++ filiasan/app_anything/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
 
-class AnythingModel(models.Model):
-    directory = models.CharField(max_length=256, null=False)
+class SeasonModel(models.Model):
+    name = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class DirectoryModel(models.Model):
+    name = models.CharField(max_length=256, null=False)
     prompt = models.CharField(max_length=1000, null=False)
-    season = models.CharField(max_length=10, null=False)
+    season = models.ForeignKey(SeasonModel, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.prompt
+
+
+class AnythingModel(models.Model):
+    directory = models.ForeignKey(DirectoryModel, null=False, on_delete=models.CASCADE)
+    image_name = models.CharField(max_length=256, null=False)
+
+    def __str__(self):
+        return self.image_name
