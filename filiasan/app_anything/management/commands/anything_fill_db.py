1172cef557868a57c6aaa362cece8121100a3211
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-10 12:51:03 +0300

    add anything_fill_db command

---
+++ filiasan/app_anything/management/commands/anything_fill_db.py
@@ -0,0 +1,51 @@
+import os
+
+from django.core.management.base import BaseCommand
+from ...models import SeasonModel, DirectoryModel, AnythingModel
+
+
+class Command(BaseCommand):
+    help = 'Fill anything database'
+
+    def handle(self, *args, **kwargs):
+        #  Anything directory\
+        #    Season directory\
+        #      Prompt directory \
+        #        prompt.txt
+        #        image1.png
+        #        image2.png
+        anything_dir = os.environ.get("ANYTHING_DIRECTORY")
+        season_dirs = [ 
+            s for s in os.listdir(anything_dir) 
+            if os.path.isdir(os.path.join(anything_dir, s)) 
+        ]
+
+        for season_dir in season_dirs:
+            season = SeasonModel.objects.filter(name=season_dir).first()
+            if not season:
+                season = SeasonModel(name=season_dir)
+                season.save()
+
+            dir = os.path.join(anything_dir, season_dir)
+            prompt_dirs = [ 
+                p for p in os.listdir(dir) 
+                if os.path.isdir(os.path.join(dir, p)) 
+            ]
+
+            for prompt_dir in prompt_dirs:
+                dir = os.path.join(anything_dir, season_dir, prompt_dir)
+
+                with open(os.path.join(dir, "prompt.txt"), "r", encoding="utf-8") as f_prompt:
+                    prompt = f_prompt.read().strip()
+
+                directory = DirectoryModel.objects.filter(name=prompt_dir, prompt=prompt).first()
+                if not directory:
+                    directory = DirectoryModel(name=prompt_dir, prompt=prompt, season=season)
+                    directory.save()
+
+                image_list = [ i for i in os.listdir(dir) if i.endswith(".png") ]
+                for image_name in image_list:
+                    anything = AnythingModel.objects.filter(name=image_name).first()
+                    if not anything:
+                        anything = AnythingModel(directory=directory, name=image_name)
+                        anything.save()
--- filiasan/app_anything/management/commands/fill_db.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from ...models import AnythingModel
-
-
-class Command(BaseCommand):
-    help = 'Fill Anything database'
-
-    def handle(self, *args, **kwargs):
-        pass
