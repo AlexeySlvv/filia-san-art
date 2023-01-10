62ca987338c7655765ff3375698fff2700180eca
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-10 13:10:34 +0300

    add season_list and season_detail to app_anything

--- filiasan/app_anything/urls.py
+++ filiasan/app_anything/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path("", views.season_list, name="season-list"),
+    path("<str:season>/", views.season_detail, name="season-detail"),
 ]
--- filiasan/app_anything/views.py
+++ filiasan/app_anything/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import SeasonModel, DirectoryModel
 
-# Create your views here.
+
+def season_list(request):
+    return render(request, "app_anything/season_list.html", context={
+        "seasons": SeasonModel.objects.all(),
+    })
+
+
+def season_detail(request, season):
+    return render(request, "app_anything/season_detail.html", context={
+        "season_name": season,
+        "directories": DirectoryModel.objects.filter(season__name=season),
+    })
