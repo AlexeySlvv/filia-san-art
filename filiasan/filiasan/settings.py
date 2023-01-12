52915773fc9b953339347defd3e788f2fb3ceffe
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-12 10:42:05 +0300

    update settings

--- filiasan/filiasan/settings.py
+++ filiasan/filiasan/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "qr*mw$$q2$nstztp54uxt$&$3)wo_htr%6_ckp_(ewhl$l8f=4"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['filia-san.ru']
+ALLOWED_HOSTS = [ "127.0.0.1", "filia-san.ru" ]
 
 
 # Application definition
