959062fc127bf5167dd3a05cf1082dd62524ddab
Alexey Solovyov <me@alexeyslvv.ru>
2023-01-12 11:26:35 +0300

    update anything urls

--- filiasan/app_anything/urls.py
+++ filiasan/app_anything/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.season_list, name="season-list"),
-    path("<str:season>/", views.season_detail, name="season-detail"),
+    path("", views.season_list, name="root-season-list"),
+    path("anything/", views.season_list, name="season-list"),
+    path("anything/<str:season>/", views.season_detail, name="season-detail"),
     path("anything/<str:season>/<str:directory>/", views.directory_detail, name="directory-detail"),
 ]
